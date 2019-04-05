192b66ab43ce499aefb1481f6ca79cd973150bd7
sirjansingh310 <sirjansingh310@gmail.com>
2019-04-05 16:13:24 +0530

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -4,7 +4,7 @@ import 'firebase/auth'
 
 //Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCXywuID55098PRfUqbVKnwO2fB6GA7-_E",
+    apiKey: "AIzaSyCXywuID55098PRfUqbVKnwO2fB6GA7-_E", // just to connect to firebase api. No one can use it to change anything on the backend without loggging into my google account.
     authDomain: "sirjansingh310-marioplan.firebaseapp.com",
     databaseURL: "https://sirjansingh310-marioplan.firebaseio.com",
     projectId: "sirjansingh310-marioplan",
